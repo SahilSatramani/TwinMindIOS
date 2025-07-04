72cead9b0e35dcc2eb3a075e658d4d4e7a3308a0
SahilSatramani <satramani.s@northeastern.edu>
2025-07-04 06:37:07 -0400

    Initial Commit

---
+++ TwinMind/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TwinMind
+//
+//  Created by Sahil Satramani on 7/4/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TwinMindTests/TwinMindTests.swift
@@ -0,0 +1,17 @@
+//
+//  TwinMindTests.swift
+//  TwinMindTests
+//
+//  Created by Sahil Satramani on 7/4/25.
+//
+
+import Testing
+@testable import TwinMind
+
+struct TwinMindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
